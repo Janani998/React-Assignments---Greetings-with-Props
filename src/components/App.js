bef5b7ffef080a83d6de68792903e973a78c8ded
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-11-08 23:22:50 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 import Welcome from "./Welcome";
 
 const App = () => {
-  return <Welcome name="name" />;
+  return <Welcome name="name of the student" />;
 };
 
 export default App;
